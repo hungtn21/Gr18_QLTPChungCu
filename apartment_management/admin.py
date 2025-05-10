7c1a9d7f60ee85bcd37270184f7b2cf33312d5ad
Tran Ngoc Hung <kiaisoft.hung.tran@gmail.com>
2025-05-10 11:14:42 +0700

    Login form finished (fake users included)

--- apartment_management/admin.py
+++ apartment_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import NguoiDung
 # Register your models here.
+
+@admin.register(NguoiDung)
+class NguoiDungAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'vai_tro', 'trang_thai')
